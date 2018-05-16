cb37a95c7590c35310b1e55895cb4ea1164f2027
Felix Tolvers <felix.tolvers@chasacademy.se>
2018-05-16 19:37:38 +0200

    finished project

--- script.js
+++ script.js
@@ -1,34 +1,58 @@
-$("#search").click(function(word){
-    $("#results img").remove();
-    $("#synonyms a").remove();
-    $.ajax({
-      url : "http://words.bighugelabs.com/api/2/3e45ddf4a8c5e0fa4a8a70395d4b2cfc/" + $("#searchterm").val() + "/json", 
-      dataType : 'json',
-      complete : function(jqXHR, textStatus) {
-        if (textStatus == 'parsererror') {
-          alert("404 error because no synonyms exist for this word");
-        }
-      },
-      success : function(data) {
+$("#search").click(function(word) {
+  $("#results img").remove();
+  $("#synonyms a").remove();
+  search($("#searchterm").val());
+  words();
+});
 
-        let synArray = data.noun.syn;
+$("body").on('click', '.word', function() {
+  $("#results img").remove();
+  $("#synonyms a").remove();
+  $("#searchterm").val($(this).text());
+  search($(this).text());
+  words();
+});
 
-        $.each(synArray, function (index, value){
-          $("#synonyms").append("<a href=https://www.flickr.com/search/?text=" + value +">" + value + ", " + "</a>");
-          if (index == 5 ) return false;
-        });
-      }
-    });
-    $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+function search(searchterm) {
+  $.getJSON(
+    "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
     {
-      tags: $("#searchterm").val(),
+      tags: searchterm,
       tagmode: "any",
       format: "json"
     },
     function(data) {
-      $.each(data.items, function(i,item){
-        $("<img/>").attr("src", item.media.m).addClass("card mb-4 box-shadow").prependTo("#results");
-        if ( i == 20 ) return false;
+      $.each(data.items, function(i, item) {
+        $("<img/>")
+          .attr("src", item.media.m)
+          .addClass("card mb-4 box-shadow")
+          .prependTo("#results");
+        if (i == 20) return false;
       });
-    });
+    }
+  );
+}
+
+function words () {
+  $.ajax({
+    url:
+      "http://words.bighugelabs.com/api/2/3e45ddf4a8c5e0fa4a8a70395d4b2cfc/" +
+      $("#searchterm").val() +
+      "/json",
+    dataType: "json",
+    complete: function(jqXHR, textStatus) {
+      if (textStatus == "parsererror") {
+        alert("404 error because no synonyms exist for this word");
+      }
+    },
+    success: function(data) {
+      let synArray = data.noun.syn;
+
+      $.each(synArray, function(index, value) {
+        $("#synonyms").append("<a class='word'>" + value + "</a>");
+        if (index == 5) return false;
+      });
+    }
   });
+
+}
